961353bf3b1ce157a618b6255d52d2c228eaeb35
satwik <raisatwik4@gmail.com>
2023-06-13 23:17:24 +0530

    change commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,7 +18,7 @@ const Banner = () => {
         </Typography>
         {/* color kuch bhi ni le skte kewal success,error aur documentation maki dekh lo */}
         <a  href='#exercises'> <Button variant="contained"  color="error" sx={{backgroundColor:'#ff2625',padding:'15px',backgroundColor:'Blue'}}>Explore Exercise</Button></a>
-        {/* <Typography fontWeight={600} color="#ff2625" sx={{opacity:0.1,display:{lg:'block',xs:'none'}}} fontSize="200px">
+        {/* <Typography fontWeight={600} color="#ff2625" sx={{opacity:0.1,display:{lg:'block',xs:'none'}}} fontSize="120px">
             Exercise
         </Typography> */}
         <img src={banner} alt="banner" className='hero-banner-img' style={{width:'750px', height:'750px',color:'blue'}}/>
